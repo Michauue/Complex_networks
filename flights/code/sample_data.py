8cc01d4b2cf745e041a335186442b7e4e0a2bc29
Michal Chylak <michauchylak@gmail.com>
2021-12-23 17:09:13 +0100

    sample data

---
+++ flights/code/sample_data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import networkx as nx
+from pandas.core import indexing
+
+df = pd.read_csv('../dataset/delays_only_v2.csv')
+df = df.sample(n=500000, random_state=254279).sort_index().reset_index()
+df = df.drop(columns=['Unnamed: 0','level_0','index'])
+df.to_csv('../dataset/sample_delays_only_v2.csv')
