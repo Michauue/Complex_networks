3b02f81c0e771605b5b582a13f67b3e7c52e018c
Michal Chylak <michauchylak@gmail.com>
2021-12-24 14:51:33 +0100

    function with 30 nice colours

---
+++ flights/code/colours.py
@@ -0,0 +1,5 @@
+def my_colours(n):
+    colours = ['aquamarine', 'burlywood', 'darksalmon', 'limegreen', 'aliceblue', 'tomato', 'lightskyblue', 'violet', 'wheat', 'sandybrown', 'orange', 'gold', 'lightcoral', 'snow', 'darkorchid',
+               'royalblue', 'teal', 'hotpink', 'chocolate', 'palegreen', 'steelblue', 'peru', 'khaki', 'mediumslateblue', 'novajowhite', 'cornflowerblue', 'moccasin', 'mediumseagreen', 'powderblue', 'rosybrown']
+    tab = colours[0:n]
+    return tab
