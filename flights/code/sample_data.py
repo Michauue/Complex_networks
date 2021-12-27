ef9d67c1463e930c16b2c31b651fdcabf9013b02
Michal Chylak <michauchylak@gmail.com>
2021-12-27 19:29:10 +0100

    update path of read file

--- flights/code/sample_data.py
+++ flights/code/sample_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import networkx as nx
 from pandas.core import indexing
 
-df = pd.read_csv('../dataset/delays_only_v2.csv')
+df = pd.read_csv('../dataset/delays_only_v3.csv')
 df = df.sample(n=500000, random_state=254279).sort_index().reset_index()
 df = df.drop(columns=['Unnamed: 0','level_0','index'])
-df.to_csv('../dataset/sample_delays_only_v2.csv')
+df.to_csv('../dataset/sample_delays_only_v3.csv')
