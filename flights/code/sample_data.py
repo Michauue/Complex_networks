b0eba516c657eeaaff91c39137c720054908471b
Michal Chylak <michauchylak@gmail.com>
2021-12-27 19:37:00 +0100

    sample data

--- flights/code/sample_data.py
+++ flights/code/sample_data.py
@@ -3,6 +3,7 @@ import networkx as nx
 from pandas.core import indexing
 
 df = pd.read_csv('../dataset/delays_only_v3.csv')
+df = df.drop(columns=['Unnamed: 0', 'level_0', 'Unnamed: 0.1', 'index'])
 df = df.sample(n=500000, random_state=254279).sort_index().reset_index()
-df = df.drop(columns=['Unnamed: 0','level_0','index'])
+df = df.drop(columns=['index'])
 df.to_csv('../dataset/sample_delays_only_v3.csv')
