4c8ca550e819da4db1a34aa8d4e86aaefae99095
Michal Chylak <michauchylak@gmail.com>
2021-12-27 16:57:20 +0100

    add desc and remove useless code

--- flights/code/6h_airline_delay.py
+++ flights/code/6h_airline_delay.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from colours import my_colours
 import numpy as np
-import networkx as nx
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('../dataset/sample_delays_only_v2.csv')
 airlines = pd.read_csv('../dataset/airlines.csv')
+
+# wykres ilości opóźnień wynoszących powyżej 6h z podziałem na linie lotnicze
 
 hour_delay = df.loc[df['DEPARTURE_DELAY'] > 300].reset_index()
 hour_delay = hour_delay.loc[hour_delay['ARRIVAL_DELAY'] > 300]
@@ -21,25 +22,3 @@ plt.bar(length, airline_to_delay['index'].values, color=my_colours(len(airline_t
 plt.xticks(length, airline_to_delay['AIRLINE'].values)
 # plt.savefig('temp_plot.png')
 plt.show()
-
-# hour_delay['CONNECTION'] = hour_delay['ORIGIN_AIRPORT']+hour_delay['DESTINATION_AIRPORT']
-
-# print(hour_delay.loc[hour_delay['CONNECTION'] == '1129814831'])
-# for i in range(len(hour_delay)):
-#     if len(hour_delay.iloc[i]['CONNECTION']) > 6:
-#         hour_delay = hour_delay.drop(index=i)
-# print(hour_delay.loc[hour_delay['CONNECTION'] == '1129814831'])
-# print(hour_delay)
-
-# edge_list=[]
-
-# for i in range(len(hour_delay['CONNECTION'])):
-#     edge_list.append([hour_delay.iloc[i]['ORIGIN_AIRPORT'],hour_delay.iloc[i]['DESTINATION_AIRPORT']])
-
-# print(edge_list)
-
-# plt.savefig('hour_delay_airport.png')
-
-# temp = hour_delay.groupby(['ORIGIN_AIRPORT']).count().sort_values('YEAR',ascending=False).head(10).reset_index()
-
-# print(temp['ORIGIN_AIRPORT'].values,temp['DEPARTURE_DELAY'].values)
