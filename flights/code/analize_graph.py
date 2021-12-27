4322d2863005f72bb30ec0adf7bde57470fc68a2
Michal Chylak <michauchylak@gmail.com>
2021-12-27 22:19:29 +0100

    add next properties to count

--- flights/code/analize_graph.py
+++ flights/code/analize_graph.py
@@ -18,9 +18,27 @@ G.add_edges_from(edge_list)
 nx.draw_kamada_kawai(G, with_labels=True)
 # plt.show()
 
-print('\nŚrednica: ',nx.diameter(G))
+print('\nŚrednica (najdłuższa z najkrótszych ścieżek): ',nx.diameter(G))
 print('\nGęstość: ',nx.density(G))
+print('\nIlość wierzchołków: ',G.number_of_nodes())
+print('\nRozmiar (ilość połączeń): ',G.size())
+
+edge_cen = pd.DataFrame.from_dict(nx.edge_betweenness_centrality(G), orient='index')
+cols_1 = ['EDGE_BETWEENNESS_CENTRALITY']
+edge_cen.columns = cols_1
+print('\nTop 5 połączeń z największym współczynnikiem centralności:\n\n',edge_cen.sort_values(by='EDGE_BETWEENNESS_CENTRALITY', ascending=False).head(5).reset_index())
+
+eig_cen = pd.DataFrame.from_dict(nx.eigenvector_centrality(G), orient='index')
+cols_2 = ['EIGENVECTOR_CENTRALITY']
+eig_cen.columns = cols_2
+print('\nTop 5 lotnisk z największym współczynnikiem centralności wektora własnego:\n\n',eig_cen.sort_values(by='EIGENVECTOR_CENTRALITY', ascending=False).head(5).reset_index())
+
+st_cen = pd.DataFrame.from_dict(nx.degree_centrality(G), orient='index')
+cols_3 = ['DEGREE_CENTRALITY']
+st_cen.columns = cols_3
+print('\nTop 5 lotnisk z największym współczynnikiem centralności:\n\n',st_cen.sort_values(by='DEGREE_CENTRALITY', ascending=False).head(5).reset_index())
+
 st = pd.DataFrame(nx.degree(G))
-cols = ['AIRPORT','CONNECTIONS']
-st.columns = cols
+cols_4 = ['AIRPORT', 'CONNECTIONS']
+st.columns = cols_4
 print('\nTop 5 lotnisk z największą ilością połączeń:\n\n',st.sort_values(by='CONNECTIONS', ascending=False).head(5).reset_index())
