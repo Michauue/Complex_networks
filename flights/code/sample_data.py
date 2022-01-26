8e0f149ddef4b14dd5f0e5e720ae54ae2fce06ce
Michal Chylak <michauchylak@gmail.com>
2022-01-26 17:26:58 +0100

    change size of sample

--- flights/code/sample_data.py
+++ flights/code/sample_data.py
@@ -4,6 +4,6 @@ from pandas.core import indexing
 
 df = pd.read_csv('../dataset/delays_only_v3.csv')
 df = df.drop(columns=['Unnamed: 0', 'level_0', 'Unnamed: 0.1', 'index'])
-df = df.sample(n=500000, random_state=254279).sort_index().reset_index()
+df = df.sample(n=50000, random_state=254279).sort_index().reset_index()
 df = df.drop(columns=['index'])
 df.to_csv('../dataset/sample_delays_only_v3.csv')
